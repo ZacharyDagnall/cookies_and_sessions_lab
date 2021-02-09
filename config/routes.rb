8427beb8e486878448047e4831b27b1a4ea19229
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-02-09 17:37:46 -0500

    commit tree 934c8984b7886cc66af6a7f6f0262233f4058976

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :products
+  root to: "products#index", as: "root"
+  get "/products", to: "products#index", as: "products"
+  post "/products", to: "products#add", as: "add"
 end
